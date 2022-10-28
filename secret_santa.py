c5bc4cb646f70a8804953713ab6c824a53cc7b7f
lmtroper <larissatroper@outlook.com>
2022-10-27 22:29:28 -0400

    cycle, pairing, rearrangement algorithm

--- secret_santa.py
+++ secret_santa.py
@@ -61,29 +61,80 @@ ux_dict = { ## come up with better name
 } 
 
 #ADD N SANTAS TO NAMES_DICT
-N = int(input("How many persons will be included in this year's Secret Santa?\n"))
-names_dict, names_list = init_santas_and_receivers(N)
-print(names_dict, '\n',names_list)
+#N = int(input("How many persons will be included in this year's Secret Santa?\n"))
+#names_dict, names_list = init_santas_and_receivers(N)
+#print(names_dict, '\n',names_list)
 #SELECT SANTA AND RECEIVER 
 # santa_dict = get_santa(N, names_dict, names_list)
 
 # print(santa_dict)
 
 
+def secret_santas(santas):
+	
+	def cycle(santas, start, count, end, matches):
+		if count + 1 > end:
+			matches[santas[end]] = santas[start]
+			return matches
+		else:
+			santa = santas[count]
+			receiver = santas[count + 1]
+			matches[santa] = receiver
+			count += 1
+			cycle(santas, start, count, end, matches)
 
 
+	def pairs(santas, matches):
+		if len(santas) == 0:
+			return matches
+		else:
+			# Choose first person and remove from list
+			n1 = randint(0,len(santas)-1)
+			santa = santas[n1]
+			santas.pop(n1)
+
+			# Choose second person and remove them from list
+			n2 = randint(0, len(santas)-1)
+			receiver = santas[n2]
+			santas.pop(n2)
+
+			# Add pairing for santa and receiver in matches
+			matches[santa] = receiver
+			matches[receiver] = santa
+
+			# Call to create next pairing
+			pairs(santas, matches)
+
+	def random_rearrange(santas):
+		N = randint(1,len(santas))
+		cycle_santas = []
+
+		if N > 1:
+			for i in range (N):
+				x = santas[randint(0,len(santas)-1)]
+				cycle_santas.append(x)
+				santas.remove(x)
+			print(cycle_santas)
+			return cycle_santas
+
+		else:
+			random_rearrange(santas)
 
 
+	matches = {}
+
+	#pairs(names, matches)
+	random_rearrange(names)
+	#num1, num2 = randint(0,len(santas)-1), randint(0,len(santas)-1)
+	#cycle(names, 0, 0, 5, matches)	
+	#print(matches)
+
+	return matches
 
 
+names = ['A', 'B', 'C', 'D', 'E', 'F']
 
-
-
-
-
-
-
-
+secret_santas(names)
 
 
 #################################################
